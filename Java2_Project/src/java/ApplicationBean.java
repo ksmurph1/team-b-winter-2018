e58d2c2214f195497f610f724d6150b45d2723dd
aflor018 <31411030+aflor018@users.noreply.github.com>
2018-02-24 20:29:22 -0800

    Add application implementation
    Initial commit for adding a new application.

--- Java2_Project/src/java/ApplicationBean.java
+++ Java2_Project/src/java/ApplicationBean.java
@@ -1,17 +1,103 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import javax.inject.Named;
-import javax.enterprise.context.Dependent;
+import javax.enterprise.context.RequestScoped;
 
 /**
  * Bean for Application
  */
 @Named(value = "applicationBean")
-@Dependent
-public class ApplicationBean {
-
+@RequestScoped
+public class ApplicationBean{
+    private static final String SQL_INSERT = "INSERT INTO Application (col1, col2, col3, col4) VALUES (?, ?, ?, ?)";
+    
+    private String appName;
+    private String appDetails;
+    private String appStatus = "Open";
+    
     /**
      * Creates a new instance of ApplicationBean
      */
     public ApplicationBean() {
     }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public String getAppDetails() {
+        return appDetails;
+    }
+
+    public String getAppStatus() {
+        return appStatus;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public void setAppDetails(String appDetails) {
+        this.appDetails = appDetails;
+    }
+
+    public void setAppStatus(String appStatus) {
+        this.appStatus = appStatus;
+    }
+    
+    /**
+     * 
+     * @throws ClassNotFoundException 
+     */
+    public void addApplication() throws ClassNotFoundException
+    {
+        Application newApp = new Application();
+        newApp.setAppName(this.appName);
+        newApp.setAppDetails(this.appDetails);
+        newApp.setAppStatus(this.appStatus);
+        int id = (int) (Math.random() * 256);
+        
+        Connection con = getConnection();
+      
+        try {   
+            PreparedStatement pst = con.prepareStatement(SQL_INSERT);
+            
+            pst.setObject(1, id);
+            pst.setObject(2, newApp.getAppName());
+            pst.setObject(3, newApp.getAppDetails());
+            pst.setObject(4, newApp.getAppStatus());
+        
+            pst.execute();
+        } catch (SQLException e) {
+        }
+    }
+    
+    /**
+     * Note: Make sure to update the url and credentials to your own
+     * 
+     * @return new database connection
+     * @throws java.lang.ClassNotFoundException
+     */
+    public Connection getConnection() throws ClassNotFoundException {
+      Connection con = null;
+      String url = "jdbc:mysql://127.0.0.1:3306/my_database";
+      String user = "root";
+      String password = "E,scky96+PJy";
+      
+      Class.forName("com.mysql.jdbc.Driver");
+      
+      try {
+         con = DriverManager.getConnection(url, user, password);
+         System.out.println("Connection completed.");
+      } catch (SQLException ex) {
+         System.out.println(ex.getMessage());
+      }
+      
+      finally {
+      }
+      return con;
+   }
     
 }
--- Java2_Project/src/java/TicketBean.java
+++ Java2_Project/src/java/TicketBean.java
@@ -1,11 +1,14 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import javax.inject.Named;
-import javax.enterprise.context.Dependent;
+import javax.enterprise.context.RequestScoped;
 
 /**
  * Bean for Ticket
  */
 @Named(value = "ticketBean")
-@Dependent
+@RequestScoped
 public class TicketBean {
 
     /**
@@ -13,5 +16,31 @@ public class TicketBean {
      */
     public TicketBean() {
     }
+    
+    /**
+     * Note: Make sure to update the url and credentials to your own
+     * 
+     * @return new database connection
+     * @throws java.lang.ClassNotFoundException
+     */
+    public Connection getConnection() throws ClassNotFoundException {
+      Connection con = null;
+      String url = "jdbc:mysql://127.0.0.1:3306/my_database";
+      String user = "root";
+      String password = "E,scky96+PJy";
+      
+      Class.forName("com.mysql.jdbc.Driver");
+      
+      try {
+         con = DriverManager.getConnection(url, user, password);
+         System.out.println("Connection completed.");
+      } catch (SQLException ex) {
+         System.out.println(ex.getMessage());
+      }
+      
+      finally {
+      }
+      return con;
+   }
     
 }
